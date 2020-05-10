578c583a2d54c487e539ee71427012ad961eeec6
Mzack9999 <marco.rivoli.nvh@gmail.com>
2020-05-10 20:25:44 +0200

    small corrections

--- chaos.go
+++ chaos.go
@@ -14,10 +14,9 @@ import (
 )
 
 var (
-	authorizationToken = flag.String("token", "", "Authorization token for API")
-	domain             = flag.String("domain", "", "Domain contains domain to find subs for")
-	statsTotal         = flag.Bool("stats-total", false, "Show total statistics")
-	stats              = flag.Bool("stats", false, "Only show statistics for the specified domain")
+	chaosKey = flag.String("chaos-key", "", "Chaos key for API")
+	domain   = flag.String("domain", "", "Domain contains domain to find subs for")
+	count    = flag.Bool("count", false, "Show statistics for the specified domain")
 )
 
 var httpclient = &http.Client{
@@ -34,54 +33,20 @@ var httpclient = &http.Client{
 func main() {
 	flag.Parse()
 
-	// Only total stats
-	if *statsTotal {
-		getTotalStats()
-		return
-	}
-
 	if *domain == "" {
 		log.Fatal("Domain not specified")
 	}
-	if *authorizationToken == "" {
+	if *chaosKey == "" {
 		log.Fatal("Authorization token not specified")
 	}
 
 	// Only domain stats
-	if *stats {
+	if *count {
 		getDomainStats()
 		return
 	}
 
 	getSubdomains()
-}
-
-func getTotalStats() {
-	req, err := http.NewRequest("GET", "https://dns.projectdiscovery.io/dns/stats", nil)
-	if err != nil {
-		log.Fatalf("Could not make request: %s\n", err)
-	}
-	resp, err := httpclient.Do(req)
-	if err != nil {
-		log.Fatalf("Could not send request: %s\n", err)
-	}
-
-	if resp.StatusCode != 200 {
-		io.Copy(ioutil.Discard, resp.Body)
-		resp.Body.Close()
-		log.Fatalf("Could not finish request: %d statuscode\n", resp.StatusCode)
-	}
-
-	var r map[string]interface{}
-	err = jsoniter.NewDecoder(resp.Body).Decode(&r)
-	if err != nil {
-		log.Fatalf("Could not unmarshal result: %s\n", err)
-	}
-
-	fmt.Printf("Total: %.0f\n", r["total"])
-	fmt.Printf("New Last Week: %.0f\n", r["new_last_week"])
-	fmt.Printf("New Last 24 Hours: %.0f\n", r["new_last_24hour"])
-	fmt.Printf("New Last Hour: %.0f\n", r["new_last_hour"])
 }
 
 func getDomainStats() {
@@ -90,7 +55,7 @@ func getDomainStats() {
 		log.Fatalf("Could not make request: %s\n", err)
 	}
 
-	req.Header.Set("Authorization", *authorizationToken)
+	req.Header.Set("Authorization", *chaosKey)
 
 	resp, err := httpclient.Do(req)
 	if err != nil {
@@ -121,7 +86,7 @@ func getSubdomains() {
 	if err != nil {
 		log.Fatalf("Could not make request: %s\n", err)
 	}
-	req.Header.Set("Authorization", *authorizationToken)
+	req.Header.Set("Authorization", *chaosKey)
 
 	resp, err := httpclient.Do(req)
 	if err != nil {
