807c7e26f9773b284bfbca0fd9d76758f09ea28f
Ice3man543 <nizamulrana@gmail.com>
2020-05-12 09:05:25 +0530

    Misc

--- chaos.go
+++ chaos.go
@@ -3,22 +3,22 @@ package main
 import (
 	"crypto/tls"
 	"flag"
-	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"time"
 
 	jsoniter "github.com/json-iterator/go"
+	"github.com/projectdiscovery/gologger"
 )
 
 var (
-	chaosKey       = flag.String("chaos-key", "", "Chaos key for API")
+	chaosKey       = flag.String("key", "", "Chaos key for API")
 	domain         = flag.String("d", "", "Domain contains domain to find subs for")
-	count          = flag.Bool("count", false, "Show statistics for the specified domain")
+	stats          = flag.Bool("stats", false, "Show statistics for the specified domain")
 	uploadfilename = flag.String("f", "", "File containing subdomains to upload")
+	silent         = flag.Bool("silent", false, "Make the output silent")
 )
 
 var httpclient = &http.Client{
@@ -35,13 +35,17 @@ var httpclient = &http.Client{
 func main() {
 	flag.Parse()
 
+	if *silent {
+		gologger.MaxLevel = gologger.Silent
+	}
+
 	// If empty try to retrieve the key from env variables
 	if *chaosKey == "" {
 		*chaosKey = os.Getenv("CHAOS_KEY")
 	}
 
 	if *chaosKey == "" {
-		log.Fatal("Authorization token not specified")
+		gologger.Fatalf("Authorization token not specified\n")
 	}
 
 	if *uploadfilename != "" {
@@ -50,11 +54,11 @@ func main() {
 	}
 
 	if *domain == "" {
-		log.Fatal("Domain not specified")
+		gologger.Fatalf("Domain not specified\n")
 	}
 
 	// Only domain stats
-	if *count {
+	if *stats {
 		getDomainStats()
 		return
 	}
@@ -65,29 +69,28 @@ func main() {
 func getDomainStats() {
 	req, err := http.NewRequest("GET", "https://dns.projectdiscovery.io/dns/"+*domain, nil)
 	if err != nil {
-		log.Fatalf("Could not make request: %s\n", err)
+		gologger.Fatalf("Could not make request: %s\n", err)
 	}
 
 	req.Header.Set("Authorization", *chaosKey)
 
 	resp, err := httpclient.Do(req)
 	if err != nil {
-		log.Fatalf("Could not send request: %s\n", err)
+		gologger.Fatalf("Could not send request: %s\n", err)
 	}
 
 	if resp.StatusCode != 200 {
 		io.Copy(ioutil.Discard, resp.Body)
 		resp.Body.Close()
-		log.Fatalf("Could not finish request: %d statuscode\n", resp.StatusCode)
+		gologger.Fatalf("Could not finish request: %d statuscode\n", resp.StatusCode)
 	}
 
 	var r map[string]interface{}
 	err = jsoniter.NewDecoder(resp.Body).Decode(&r)
 	if err != nil {
-		log.Fatalf("Could not unmarshal result: %s\n", err)
+		gologger.Fatalf("Could not unmarshal result: %s\n", err)
 	}
-
-	fmt.Println (r["subdomains"])
+	gologger.Silentf("%s\n", r["subdomains"])
 }
 
 type result struct {
@@ -97,30 +100,30 @@ type result struct {
 func getSubdomains() {
 	req, err := http.NewRequest("GET", "https://dns.projectdiscovery.io/dns/"+*domain+"/subdomains", nil)
 	if err != nil {
-		log.Fatalf("Could not make request: %s\n", err)
+		gologger.Fatalf("Could not make request: %s\n", err)
 	}
 	req.Header.Set("Authorization", *chaosKey)
 
 	resp, err := httpclient.Do(req)
 	if err != nil {
-		log.Fatalf("Could not send request: %s\n", err)
+		gologger.Fatalf("Could not send request: %s\n", err)
 	}
 
 	if resp.StatusCode != 200 {
 		io.Copy(ioutil.Discard, resp.Body)
 		resp.Body.Close()
-		log.Fatalf("Could not finish request: %d statuscode\n", resp.StatusCode)
+		gologger.Fatalf("Could not finish request: %d statuscode\n", resp.StatusCode)
 	}
 
 	r := result{}
 	err = jsoniter.NewDecoder(resp.Body).Decode(&r)
 	if err != nil {
-		log.Fatalf("Could not unmarshal result: %s\n", err)
+		gologger.Fatalf("Could not unmarshal result: %s\n", err)
 	}
 
 	for _, subdomain := range r.Subdomains {
 		if subdomain != "" {
-			fmt.Println(subdomain + "." + *domain)
+			gologger.Silentf("%s.%s\n", subdomain, *domain)
 		}
 	}
 }
@@ -128,25 +131,25 @@ func getSubdomains() {
 func uploadFile() {
 	file, err := os.Open(*uploadfilename)
 	if err != nil {
-		log.Fatal(err)
+		gologger.Fatalf("Could not open subdomains file: %s\n", err)
 	}
 
 	req, err := http.NewRequest("POST", "https://dns.projectdiscovery.io/dns/add", file)
 	if err != nil {
-		log.Fatalf("Could not make request: %s\n", err)
+		gologger.Fatalf("Could not make request: %s\n", err)
 	}
 	req.Header.Set("Authorization", *chaosKey)
 
 	resp, err := httpclient.Do(req)
 	if err != nil {
-		log.Fatalf("Could not send request: %s\n", err)
+		gologger.Fatalf("Could not send request: %s\n", err)
 	}
 
 	if resp.StatusCode != 200 {
 		io.Copy(ioutil.Discard, resp.Body)
 		resp.Body.Close()
-		log.Fatalf("Could not finish request: %d statuscode\n", resp.StatusCode)
+		gologger.Fatalf("Could not finish request: %d statuscode\n", resp.StatusCode)
 	}
 
-	log.Println("File processed successfully and subdomains with valid records will be updated to chaos dataset.")
+	gologger.Infof("File processed successfully and subdomains with valid records will be updated to chaos dataset.")
 }
