82ea32dd8c9000b252aa0b34a36ea68708cb2ddf
bauthard <8293321+bauthard@users.noreply.github.com>
2020-07-05 15:42:51 +0530

    Update banner.go

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -11,7 +11,7 @@ const banner = `
 `
 
 // Version is the current version of chaos
-const Version = `0.1.4`
+const Version = `0.1.5`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
