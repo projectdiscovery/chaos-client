ffa4113daaf351e5f28ea73cd914e4240628afec
Sandeep Singh <sandeep@projectdiscovery.io>
2023-03-07 19:43:31 +0530

    Update runs-on to ubuntu-latest-16-cores and Go version to 1.19 in all workflows and update version in banner and goreleaser configs. (#156)

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -7,17 +7,14 @@ const banner = `
   _____/ /_  ____ _____  _____
  / ___/ __ \/ __  / __ \/ ___/
 / /__/ / / / /_/ / /_/ (__  ) 
-\___/_/ /_/\__,_/\____/____/  v0.4.0
+\___/_/ /_/\__,_/\____/____/  v0.5.0
 `
 
 // Version is the current version of chaos
-const Version = `0.4.0`
+const Version = `0.5.0`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
 	gologger.Print().Msgf("%s\n", banner)
-	gologger.Print().Msgf("\t\tprojectdiscovery.io\n\n")
-
-	gologger.Print().Msgf("Use with caution. You are responsible for your actions\n")
-	gologger.Print().Msgf("Developers assume no liability and are not responsible for any misuse or damage.\n")
+	gologger.Print().Msgf("\t\tchaos.projectdiscovery.io\n\n")
 }
