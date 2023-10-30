ecf1c61aa1565c785bd10c4bb5beaf085756086f
Maxim Bogdanov <muravlion@gmail.com>
2023-10-30 13:24:27 +0100

    all

--- cmd/chaos/chaos.go
+++ cmd/chaos/chaos.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"os"
+
 	"github.com/alecthomas/kong"
 	"github.com/projectdiscovery/chaos-client/internal"
+	"github.com/projectdiscovery/chaos-client/internal/dns"
 	"github.com/projectdiscovery/chaos-client/internal/subdomains"
 	"github.com/projectdiscovery/gologger"
 	"github.com/projectdiscovery/gologger/levels"
 	updateutils "github.com/projectdiscovery/utils/update"
-	"os"
 )
 
 var cli struct {
@@ -22,18 +23,21 @@ var cli struct {
 	Subdomains         struct {
 		Domain string `arg:"" help:"Domain to search for subdomains"`
 		Count  bool   `help:"Show statistics for the specified domain"`
-		DL     string `help:"File containing domains to search for subdomains (optional)" aliases:"dL"`
 		JSON   bool   `help:"Print output as json"`
 	} `cmd:"" help:"List subdomains"`
-
+	SubdomainsBatch struct {
+		File string `arg:"" help:"File containing domains to search for subdomains"`
+		JSON bool   `help:"Print output as json"`
+	} `cmd:"" help:"List subdomains from file"`
 	DNS struct {
-		Paths []string `arg:"" optional:"" help:"Paths to list." type:"path"`
+		Domain string           `arg:"" help:"Domain to search for DNS record"`
+		Types  []dns.RecordType `help:"DNS record type(s) (a,aaaa,caa,cname,mx,ns,soa,srv,txt)" short:"t"`
 	} `cmd:"" help:"Get DNS record"`
 }
 
 func main() {
 	ctx := kong.Parse(&cli,
-		kong.Name("Chaos"),
+		kong.Name("chaos"),
 		kong.Description("Chaos client"),
 		kong.UsageOnError(),
 		kong.ConfigureHelp(kong.HelpOptions{
@@ -65,21 +69,56 @@ func main() {
 	switch ctx.Command() {
 	case "subdomains <domain>":
 		opts := subdomains.Options{
-			APIKey:             cli.Key,
-			Domain:             cli.Subdomains.Domain,
-			Count:              cli.Subdomains.Count,
-			Silent:             cli.Silent,
-			Output:             cli.Output,
-			DomainsFile:        cli.Subdomains.DL,
-			JSONOutput:         cli.Subdomains.JSON,
-			Version:            cli.Version,
-			Verbose:            cli.Verbose,
-			DisableUpdateCheck: cli.DisableUpdateCheck,
+			APIKey:     cli.Key,
+			Count:      cli.Subdomains.Count,
+			Silent:     cli.Silent,
+			Output:     cli.Output,
+			JSONOutput: cli.Subdomains.JSON,
+			Version:    cli.Version,
+			Verbose:    cli.Verbose,
 		}
-		opts.ValidateOptions()
-		subdomains.RunEnumeration(&opts)
+		err := opts.ValidateOptions()
+		if err != nil {
+			gologger.Fatal().Msg(err.Error())
+		}
+		err = subdomains.Run(&opts)
+		if err != nil {
+			gologger.Fatal().Msg(err.Error())
+		}
+	case "subdomains-batch <file>":
+		opts := subdomains.Options{
+			APIKey:      cli.Key,
+			DomainsFile: cli.SubdomainsBatch.File,
+			Silent:      cli.Silent,
+			Output:      cli.Output,
+			JSONOutput:  cli.SubdomainsBatch.JSON,
+			Version:     cli.Version,
+			Verbose:     cli.Verbose,
+		}
+		err := opts.ValidateOptions()
+		if err != nil {
+			gologger.Fatal().Msg(err.Error())
+		}
+		err = subdomains.Run(&opts)
+		if err != nil {
+			gologger.Fatal().Msg(err.Error())
+		}
+	case "dns <domain>":
+		opts := dns.Options{
+			APIKey: cli.Key,
+			Domain: cli.DNS.Domain,
+			Types:  cli.DNS.Types,
+		}
+		err := opts.ValidateOptions()
+		if err != nil {
+			gologger.Fatal().Msg(err.Error())
+		}
+
+		err = dns.Run(&opts)
+		if err != nil {
+			gologger.Fatal().Msg(err.Error())
+		}
 	default:
-		fmt.Printf("unexpected command")
-		os.Exit(1)
+		gologger.Fatal().Msgf("unexpected command: %s", ctx.Command())
 	}
 }
