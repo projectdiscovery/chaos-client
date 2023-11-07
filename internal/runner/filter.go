7bfdee60561a8c6b295ccdf009d8bf80122201f5
Maxim Bogdanov <muravlion@gmail.com>
2023-11-07 13:57:54 +0100

    Revert "DNS feature and refactoring for V2 (#205)"
    This reverts commit 610a5d488647bcbc39e67ab121d2aa4effba1c19.

---
+++ internal/runner/filter.go
@@ -0,0 +1,145 @@
+package runner
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/projectdiscovery/chaos-client/pkg/chaos"
+)
+
+type DNSStatusCode int
+
+const (
+	NOERROR DNSStatusCode = iota
+	NXDOMAIN
+	SERVFAIL
+	REFUSED
+	ANYDNSCODE
+)
+
+type DNSRecordType int
+
+const (
+	A DNSRecordType = iota
+	AAAA
+	CNAME
+	NS
+	ANYRECORDTYPE
+)
+
+type Filter struct {
+	DNSStatusCode       DNSStatusCode
+	DNSRecordType       DNSRecordType
+	FilterWildcard      bool
+	Response            bool
+	ResponseOnly        bool
+	HTTPUrl             bool
+	HTTPTitle           bool
+	HTTPStatusCode      bool
+	HTTPStatusCodeValue int
+	HTTPContentLength   bool
+}
+
+func (f *Filter) isHTTPRequested() bool {
+	return f.HTTPUrl || f.HTTPTitle || f.HTTPStatusCode || f.HTTPContentLength
+}
+
+func applyFilter(data *chaos.BBQData, filter *Filter) bool {
+	// wildcard
+	if filter.FilterWildcard && data.Wildcard {
+		return false
+	}
+	// dns status code
+	if filter.DNSStatusCode == NOERROR && data.StatusCode != "NOERROR" {
+		return false
+	}
+	if filter.DNSStatusCode == NXDOMAIN && data.StatusCode != "NXDOMAIN" {
+		return false
+	}
+	if filter.DNSStatusCode == SERVFAIL && data.StatusCode != "SERVFAIL" {
+		return false
+	}
+	if filter.DNSStatusCode == REFUSED && data.StatusCode != "REFUSED" {
+		return false
+	}
+
+	// dns record type
+	if filter.DNSRecordType == A && len(data.A) == 0 {
+		return false
+	}
+	if filter.DNSRecordType == AAAA && len(data.AAAA) == 0 {
+		return false
+	}
+	if filter.DNSRecordType == CNAME && len(data.CNAME) == 0 {
+		return false
+	}
+	if filter.DNSRecordType == NS && len(data.NS) == 0 {
+		return false
+	}
+
+	// http status code
+	if filter.HTTPStatusCodeValue > 0 && filter.HTTPStatusCodeValue != data.HTTPStatusCode {
+		return false
+	}
+
+	return true
+}
+
+func extractOutput(data *chaos.BBQData, filter *Filter) string {
+	// dns - response
+	if filter.Response {
+		switch filter.DNSRecordType {
+		case A:
+			return strings.Join(prefixWith(data.A, data.Subdomain+"."+data.Domain), "\n")
+		case AAAA:
+			return strings.Join(prefixWith(data.AAAA, data.Subdomain+"."+data.Domain), "\n")
+		case CNAME:
+			return strings.Join(prefixWith(data.CNAME, data.Subdomain+"."+data.Domain), "\n")
+		case NS:
+			return strings.Join(prefixWith(data.NS, data.Subdomain+"."+data.Domain), "\n")
+		}
+	}
+
+	if filter.ResponseOnly {
+		switch filter.DNSRecordType {
+		case A:
+			return strings.Join(data.A, "\n")
+		case AAAA:
+			return strings.Join(data.AAAA, "\n")
+		case CNAME:
+			return strings.Join(data.CNAME, "\n")
+		case NS:
+			return strings.Join(data.NS, "\n")
+		}
+	}
+
+	if filter.isHTTPRequested() {
+		// http - flags
+		httpbuf := data.HTTPUrl
+		if filter.HTTPStatusCode {
+			httpbuf += fmt.Sprintf(" [%d]", data.HTTPStatusCode)
+		}
+		if filter.HTTPContentLength {
+			httpbuf += fmt.Sprintf(" [%d]", data.HTTPContentLength)
+		}
+		if filter.HTTPTitle {
+			httpbuf += fmt.Sprintf(" [%s]", data.HTTPTitle)
+		}
+		// if the url has been requested or some data added to the base one
+		if (filter.HTTPUrl || len(httpbuf) != len(data.HTTPUrl)) && len(data.HTTPUrl) > 0 {
+			return httpbuf
+		}
+		return ""
+	}
+
+	// default - print subdomain
+	return fmt.Sprintf("%s.%s", data.Subdomain, data.Domain)
+}
+
+func prefixWith(s []string, prefix string) []string {
+	for i, ss := range s {
+		s[i] = fmt.Sprintf("%s %s", prefix, ss)
+	}
+
+	return s
+}
---
+++ internal/runner/runner.go
@@ -0,0 +1,139 @@
+package runner
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/projectdiscovery/chaos-client/pkg/chaos"
+	"github.com/projectdiscovery/gologger"
+)
+
+// RunEnumeration runs the enumeration for Chaos client
+func RunEnumeration(opts *Options) {
+	client := chaos.New(opts.APIKey)
+
+	if opts.Count {
+		resp, err := client.GetStatistics(&chaos.GetStatisticsRequest{
+			Domain: opts.Domain,
+		})
+		if err != nil {
+			gologger.Fatal().Msgf("Could not get statistics: %s\n", err)
+		}
+		gologger.Silent().Msgf("%d\n", resp.Subdomains)
+		return
+	}
+
+	outputWriters := []io.Writer{os.Stdout}
+	if opts.Output != "" {
+		var err error
+		opts.outputFile, err = os.Create(opts.Output)
+		if err != nil {
+			gologger.Fatal().Msgf("Could not create file %s for %s: %s\n", opts.Output, opts.Domain, err)
+		}
+		defer opts.outputFile.Close()
+		outputWriters = append(outputWriters, opts.outputFile)
+	}
+	opts.outputWriter = io.MultiWriter(outputWriters...)
+
+	if opts.Domain != "" {
+		if opts.BBQ {
+			processBBQDomain(client, opts)
+		} else {
+			processDomain(client, opts)
+		}
+	}
+
+	if opts.hasStdin() || opts.DomainsFile != "" {
+		processList(client, opts)
+	}
+}
+
+func processDomain(client *chaos.Client, opts *Options) {
+	req := &chaos.SubdomainsRequest{Domain: opts.Domain}
+	if opts.JSONOutput {
+		req.OutputFormat = "json"
+	}
+	for item := range client.GetSubdomains(req) {
+		if item.Error != nil {
+			gologger.Error().Msgf("Could not get subdomains for %s: %s\n", opts.Domain, item.Error)
+			break
+		}
+		if opts.JSONOutput {
+			_, _ = io.Copy(opts.outputWriter, *item.Reader)
+		} else {
+			if item.Subdomain != "" {
+				gologger.Silent().Msgf("%s.%s\n", item.Subdomain, opts.Domain)
+			}
+			if opts.Output != "" {
+				_, err := fmt.Fprintf(opts.outputWriter, "%s.%s\n", item.Subdomain, opts.Domain)
+				if err != nil {
+					gologger.Error().Msgf("Could not write results to file %s for %s: %s\n", opts.Output, opts.Domain, err)
+					break
+				}
+			}
+		}
+	}
+}
+
+func processBBQDomain(client *chaos.Client, opts *Options) {
+	req := &chaos.SubdomainsRequest{Domain: opts.Domain}
+	if opts.JSONOutput {
+		req.OutputFormat = "json"
+	}
+	var outputLine string
+	for item := range client.GetBBQSubdomains(req) {
+		if item.Error != nil {
+			gologger.Error().Msgf("Could not get subdomains: %s\n", item.Error)
+			break
+		}
+		var bbqdata chaos.BBQData
+		if opts.JSONOutput {
+			_, _ = io.Copy(opts.outputWriter, *item.Reader)
+		} else {
+			if err := json.Unmarshal(item.Data, &bbqdata); err != nil {
+				gologger.Error().Msgf("Could not unmarshal response: %s\n", err)
+				break
+			}
+			// filters
+			if !applyFilter(&bbqdata, opts.filter) {
+				continue
+			}
+			outputLine = fmt.Sprint(extractOutput(&bbqdata, opts.filter))
+			if outputLine != "" {
+				_, err := fmt.Fprintf(opts.outputWriter, "%s\n", outputLine)
+				if err != nil {
+					gologger.Error().Msgf("Could not write results to file %s for %s: %s\n", opts.Output, opts.Domain, err)
+					break
+				}
+			}
+		}
+	}
+}
+
+func processList(client *chaos.Client, opts *Options) {
+	var file *os.File
+	var err error
+
+	if opts.hasStdin() {
+		file = os.Stdin
+	}
+	if opts.DomainsFile != "" {
+		file, err = os.Open(opts.DomainsFile)
+	}
+	if err != nil {
+		gologger.Fatal().Msgf("Could not read domain list: %s\n", err)
+	}
+
+	in := bufio.NewScanner(file)
+	for in.Scan() {
+		opts.Domain = in.Text()
+		if opts.BBQ {
+			processBBQDomain(client, opts)
+		} else {
+			processDomain(client, opts)
+		}
+	}
+}
